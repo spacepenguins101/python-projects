7f3f1a510e78e689c0f1a67b2cd71ca626d54f5c
Javier Lim <spacepngn101@gmail.com>
2023-12-15 16:58:21 +0800

    Add basic project information

---
+++ 1 Expense Tracker/ExpenseTracker.py
@@ -0,0 +1,12 @@
+'''
+Project Aim
+-----------
+This project seeks to help people track their expenses better and faster.
+
+Basic Functionality
+-------------------
+1. Add expenses
+2. View specific expenses
+3. Edit and delete expenses
+4. Calculate statistics
+'''
